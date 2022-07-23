955c3e558447c458142d5e67fd4f671cde88f585
Leejuneyeong <rytn1702@naver.com>
2022-07-23 20:18:06 +0900

    python

---
+++ Module.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#더하기 해주는 함수 
+def add_num (num1, num2) : 
+    return num1+num2
+
+
+# In[4]:
+
+
+# 빼기 해주는 함수
+def add_sub (num1, num2) :
+    return num1-num2
+
+
+# In[1]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
